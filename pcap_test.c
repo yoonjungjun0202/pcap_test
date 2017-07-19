4af90900a812ca7c89936545854d3fce7d5f0f15
yoonjungjun0202 <love667@naver.com>
2017-07-19 19:53:16 +0900

    Modify pcap_test, add structure

--- pcap_test.c
+++ pcap_test.c
@@ -1,94 +1,176 @@
-#include <pcap.h>
 #include <stdio.h>
+#include <pcap.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
 
+#define MAC_SIZE 6
 
-int print_to_eth(const u_char *_packet, int _offset)
+/* define struct */
+struct ethernet_s
 {
-	int i;
-	const u_char *eth = _packet + _offset;
+	uint8_t dst_mac[MAC_SIZE];
+	uint8_t src_mac[MAC_SIZE];
+	uint16_t type;
+};
 
+struct ip_s
+{
+	uint8_t vhl;
+	uint8_t tos;
+	uint16_t len;
+	uint16_t id;
+	uint16_t flag;
+	uint8_t ttl;
+	uint8_t proto;
+	uint16_t checksum;
+	struct in_addr src_ip;
+	struct in_addr dst_ip;
+};
 
-	printf("dmac: ");
-	for(i=0; i<5; i++)
-		printf("%02X:", eth[i]);
-	printf("%02X\n", eth[i]);
+struct tcp_s
+{
+	uint16_t src_port;
+	uint16_t dst_port;
+	uint32_t seq_num;
+	uint32_t ack_num;
+	uint8_t off_res;
+	uint8_t flag;
+	uint16_t window;
+	uint16_t checksum;
+	uint16_t urg_point;
+};
 
-	printf("smac: ");
-	for(i=6; i<11; i++)
-		printf("%02X:", eth[i]);
-	printf("%02X\n", eth[i]);
+struct data_s
+{
+	int length;
+	u_char *data;
+};
 
+typedef struct ethernet_s ethernet_t[1];
+typedef struct ip_s ip_t[1];
+typedef struct tcp_s tcp_t[1];
+typedef struct data_s data_t[1];
 
-	return 14;
+#define IP_VERSION(x) ((x & 0xf0) >> 4)	// (ip->vhl & 0xf0) >> 4
+#define IP_LENGTH(x) ((x & 0x0f) << 2)	// (ip->vhl & 0x0f) << 2
+#define TCP_OFFSET(x) ((x & 0xf0) >> 2)	// ((tcp->off_res & 0xf0) >> 4) << 2)
+#define TCP_RESERVED(x) (x & 0x0f)		// tcp->off_res & 0x0f
+
+int set_ethernet(ethernet_t _eth, const u_char *_packet)
+{
+	memcpy(_eth, _packet, sizeof(struct ethernet_s));
+	return sizeof(_eth[0]);
 }
 
-int print_to_ip(const u_char *_packet, int _offset)
+int set_ip(ip_t _ip, const u_char *_packet, int _offset)
 {
-	int i;
-	const u_char *ip = _packet + _offset;
-
-
-	printf("sip: ");
-	for(i=12; i<15; i++)
-		printf("%d.", ip[i]);
-	printf("%d\n", ip[i]);
-
-	printf("dip: ");
-	for(i=16; i<19; i++)
-		printf("%d.", ip[i]);
-	printf("%d\n", ip[i]);
-
-
-	return (int)(ip[0] & 0x0f) * 4;
+	memcpy(_ip, _packet + _offset, sizeof(struct ip_s));
+	return IP_LENGTH(_ip->vhl);
 }
 
-int print_to_port(const u_char *_packet, int _offset)
+int set_tcp(tcp_t _tcp, const u_char *_packet, int _offset)
 {
-	const u_char *port = _packet + _offset;
-
-
-	printf("sport: %d\n", (((int)port[0] << 8) | (int)port[1]));
-	printf("dport: %d\n", (((int)port[2] << 8) | (int)port[3]));
-
-
-	return (int)((port[12] & 0xf0) >> 4) * 4;
+	memcpy(_tcp, _packet + _offset, sizeof(struct tcp_s));
+	return TCP_OFFSET(_tcp->off_res);
 }
 
-int print_to_data(const u_char *_packet, int _offset, int _len)
+int set_data(data_t _data, const u_char *_packet, int _offset, int _data_length)
 {
-	int i;
-	const u_char *data = _packet + _offset;
+	if(0 == _data_length)
+		return 0;
 
-
-	printf("## data\n");
-	for(i=0; i<_len; i++)
-		printf("%02X ", data[i]);
-	printf("\n");
-	for(i=0; i<_len; i++)
-		printf("%c ", data[i]);
-	printf("\n");
-
-
-	return 0;
+	_data->length = _data_length+1;
+	_data->data = (u_char *) calloc (_data->length * sizeof(u_char), sizeof(u_char));
+	memcpy(_data->data, _packet + _offset, _data_length);
+	return _data_length;
 }
 
-int main(int argc, char *argv[])
+int packet_parsing(ethernet_t _eth, ip_t _ip, tcp_t _tcp, data_t _data, const u_char *_packet)
 {
-	pcap_t *handle;			/* Session handle */
-	char *dev;			/* The device to sniff on */
+	int data_length = 0;
+	int offset = 0;
+
+	offset += set_ethernet(_eth, _packet);
+	if(ETHERTYPE_IP != ntohs(_eth->type))
+		return 1;
+
+	offset += set_ip(_ip, _packet, offset);
+	if(IPPROTO_TCP != _ip->proto)
+		return 2;
+
+	offset += set_tcp(_tcp, _packet, offset);
+	data_length = _ip->len - TCP_OFFSET(_tcp->off_res);
+	if(0 == data_length)
+		return 3;
+
+	set_data(_data, _packet, offset, data_length);
+
+	return 4;
+}
+
+void print_eth_info(ethernet_t _eth)
+{
+	printf("##### Ethernet Info #####\n");
+	printf(">> dst mac : %s\n", ether_ntoa((struct ether_addr *)_eth->dst_mac));
+	printf(">> src mac : %s\n", ether_ntoa((struct ether_addr *)_eth->src_mac));
+}
+
+void print_ip_info(ip_t _ip)
+{
+	char buf[20] = {'\0', };
+	printf("##### IP Info #####\n");
+	inet_ntop(AF_INET, &(_ip->src_ip), buf, sizeof(buf));
+	printf(">> src IP : %s\n", buf);
+	inet_ntop(AF_INET, &(_ip->dst_ip), buf, sizeof(buf));
+	printf(">> dst IP : %s\n", buf);
+}
+
+void print_tcp_info(tcp_t _tcp)
+{
+	printf("##### TCP Info #####\n");
+	printf(">> src TCP port: %d\n", ntohs(_tcp->src_port));
+	printf(">> src TCP port: %d\n", ntohs(_tcp->dst_port));
+}
+
+void print_data_info(data_t _data)
+{
+	printf("##### data Info #####\n");
+	printf("%s\n", _data->data);
+}
+
+void print_packet_info(ethernet_t _eth, ip_t _ip, tcp_t _tcp, data_t _data, int _lev)
+{
+	if(0 >= _lev--)
+		return;
+	print_eth_info(_eth);
+
+	if(0 >= _lev--)
+		return;
+	print_ip_info(_ip);
+
+	if(0 >= _lev--)
+		return;
+	print_tcp_info(_tcp);
+
+	if(0 >= _lev--)
+		return;
+	print_data_info(_data);
+}
+
+void init_pcap(pcap_t **handle, char *_filter_exp, char *_dev)
+{
+	char *dev = _dev;			/* The device to sniff on */
 	char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
 	struct bpf_program fp;		/* The compiled filter */
-	char filter_exp[] = "port 80";	/* The filter expression */
 	bpf_u_int32 mask;		/* Our netmask */
 	bpf_u_int32 net;		/* Our IP */
-	struct pcap_pkthdr *header;	/* The header that pcap gives us */
-	const u_char *packet;		/* The actual packet */
 
-	
-	int re, offset;
-
-
-	/* Define the device */
+/*
+	// Define the device
 	// dev = wlan0
 	dev = pcap_lookupdev(errbuf);
 	if (dev == NULL) {
@@ -96,29 +178,50 @@ int main(int argc, char *argv[])
 		return(2);
 	}
 	printf("device is %s\n", dev);
-	/* Find the properties for the device */
+	// Find the properties for the device
 	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
 		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
 		net = 0;
 		mask = 0;
 	}
+*/
 	/* Open the session in promiscuous mode */
 	// file open about wlan0
-	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-	if (handle == NULL) {
+	*handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	*handle = pcap_open_live("awdl0", BUFSIZ, 1, 1000, errbuf);	// dump
+	if (*handle == NULL) {
 		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-		return(2);
+		exit(2);
 	}
 	/* Compile and apply the filter */
-	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-		return(2);
+	if (pcap_compile(*handle, &fp, _filter_exp, 0, net) == -1) {
+		fprintf(stderr, "Couldn't parse filter %s: %s\n", _filter_exp, pcap_geterr(*handle));
+		exit(2);
 	}
-	if (pcap_setfilter(handle, &fp) == -1) {
-		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
-		return(2);
+	if (pcap_setfilter(*handle, &fp) == -1) {
+		fprintf(stderr, "Couldn't install filter %s: %s\n", _filter_exp, pcap_geterr(*handle));
+		exit(2);
 	}
+}
 
+int main(int argc, char *argv[])
+{
+	pcap_t *handle;			/* Session handle */
+	char filter_exp[] = "port 80";	/* The filter expression */
+	struct pcap_pkthdr *header;	/* The header that pcap gives us */
+	const u_char *packet;		/* The actual packet */
+
+	
+	int re, offset;
+	ethernet_t eth;
+	ip_t ip;
+	tcp_t tcp;
+	data_t data;
+
+	if(argc < 2)
+		return 0;
+
+	init_pcap(&handle, filter_exp, argv[1]);
 
 
 	/* Grab a packet */
@@ -127,13 +230,10 @@ int main(int argc, char *argv[])
 		if( 0 == re )
 			continue;
 
-		printf("## info\n");
-		offset = 0;
-		offset += print_to_eth(packet, offset);
-		offset += print_to_ip(packet, offset);
-		offset += print_to_port(packet, offset);
-		offset += print_to_data(packet, offset, 12);
-
+		int lev = packet_parsing(eth, ip, tcp, data, packet);
+		print_packet_info(eth, ip, tcp, data, lev);
+		if(lev == 4)
+			free(data->data);
 		printf("\n\n");
 		/* And close the session */
 	}
